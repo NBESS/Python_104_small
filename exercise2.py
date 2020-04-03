fd1e36ed5f4dec00f25ab6196b7afa8b4e2efc0e
NBESS <nicholasdbess@gmail.com>
2020-04-03 15:26:06 -0400

    adds code to complete task

---
+++ exercise2.py
@@ -0,0 +1,9 @@
+# Using a for loop, print the even numbers from 0 to 99
+
+# Setup
+# Create a loop that prints all numbers in desired range
+# iterate to display even numbers
+
+for num in range(0, 100, 2):
+    # Result/ Output
+    print(num)
