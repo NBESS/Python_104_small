43ce527c5f679f1054e6ae02ab5a1f33f7ea4755
NBESS <nicholasdbess@gmail.com>
2020-04-03 14:59:53 -0400

    adds code for loop

--- python_exercise1.py
+++ python_exercise1.py
@@ -1,12 +1,8 @@
-# Print the numbers from 0 to 99
+# Using a for loop, print the numbers from 0 to 99
 
 # Setup
+# Create a loop that prints all numbers in desired range
 
-
-
-# Do the work
-
-
-
-
-# Result/ Output
+for num in range(0, 99):
+    # Result/ Output
+    print(num)
